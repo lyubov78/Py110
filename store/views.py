3d95cb66c06dde58bc72b3e9434d3db3558a1c58
lyuba <lyubabelousova@gmail.com>
2023-12-25 13:28:35 +0300

    my fourth practice

--- store/views.py
+++ store/views.py
@@ -77,7 +77,7 @@ def cart_view(request):
             product = dict(DATABASE[product_id])  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
             # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
             product['quantity'] = quantity
-            product["price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            product['price_total'] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
             # 3. добавьте product в список products
             products.append(product)
 
