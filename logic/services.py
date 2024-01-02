5a068e3834410070fd7eb0e6b11ca9f5419f05e6
Lyubov <144713098+lyubov78@users.noreply.github.com>
2024-01-02 17:08:54 +0300

    my fifth practice

--- logic/services.py
+++ logic/services.py
@@ -1,6 +1,7 @@
 import json
 import os
 from store.models import DATABASE
+from django.contrib.auth import get_user
 
 
 def filtering_category(database: dict,
@@ -39,7 +40,7 @@ def filtering_category(database: dict,
     return result
 
 
-def view_in_cart() -> dict:
+def view_in_cart(request) -> dict:
     """
     Просматривает содержимое cart.json
 
@@ -49,14 +50,15 @@ def view_in_cart() -> dict:
         with open('cart.json', encoding='utf-8') as f:
             return json.load(f)
 
-    cart = {'products': {}}  # Создаём пустую корзину
+    user = get_user(request).username  # Получаем авторизированного пользователя
+    cart = {user: {'products': {}}}  # Создаём пустую корзину
     with open('cart.json', mode='x', encoding='utf-8') as f:   # Создаём файл и записываем туда пустую корзину
         json.dump(cart, f)
 
     return cart
 
 
-def add_to_cart(id_product: str) -> bool:
+def add_to_cart(request, id_product: str) -> bool:
     """
     Добавляет продукт в корзину. Если в корзине нет данного продукта, то добавляет его с количеством равное 1.
     Если в корзине есть такой продукт, то добавляет количеству данного продукта + 1.
@@ -65,7 +67,8 @@ def add_to_cart(id_product: str) -> bool:
     :return: Возвращает True в случае успешного добавления, а False в случае неуспешного добавления(товара по id_product
     не существует).
     """
-    cart = view_in_cart()
+    cart_users = view_in_cart(request)
+    cart = cart_users[get_user(request).username]
 
     # TODO Проверьте, а существует ли такой товар в корзине, если нет, то перед тем как его добавить - проверьте есть ли такой
     # id товара в вашей базе данных DATABASE, чтобы уберечь себя от добавления несуществующего товара.
@@ -78,14 +81,14 @@ def add_to_cart(id_product: str) -> bool:
                 cart['products'][id_product] = 1
             else:
                 cart['products'][id_product] += 1
-            json.dump(cart, f)
+            json.dump(cart_users, f)
     else:
         return False
 
     return True
 
 
-def remove_from_cart(id_product: str) -> bool:
+def remove_from_cart(request, id_product: str) -> bool:
     """
     Добавляет позицию продукта из корзины. Если в корзине есть такой продукт, то удаляется ключ в словаре
     с этим продуктом.
@@ -94,7 +97,8 @@ def remove_from_cart(id_product: str) -> bool:
     :return: Возвращает True в случае успешного удаления, а False в случае неуспешного удаления(товара по id_product
     не существует).
     """
-    cart = view_in_cart()  # TODO Помните, что у вас есть уже реализация просмотра корзины,
+    cart_users = view_in_cart(request)
+    cart = cart_users[get_user(request).username]  # TODO Помните, что у вас есть уже реализация просмотра корзины,
     # поэтому, чтобы загрузить данные из корзины, не нужно заново писать код.
 
     # С переменной cart функции remove_from_cart ситуация аналогичная, что с cart функции add_to_cart
@@ -105,11 +109,28 @@ def remove_from_cart(id_product: str) -> bool:
     if id_product in cart['products']:
         with open('cart.json', 'w', encoding='utf-8') as f:
             del cart['products'][id_product]
-            json.dump(cart, f)
+            json.dump(cart_users, f)
     else:
         return False
 
     return True
+
+
+def add_user_to_cart(request, username: str) -> None:
+    """
+    Добавляет пользователя в базу данных корзины, если его там не было.
+
+    :param username: Имя пользователя
+    :return: None
+    """
+    cart_users = view_in_cart(request)  # Чтение всей базы корзин
+
+    cart = cart_users.get(username)  # Получение корзины конкретного пользователя
+
+    if not cart:  # Если пользователя до настоящего момента не было в корзине, то создаём его и записываем в базу
+        with open('cart.json', mode='w', encoding='utf-8') as f:
+            cart_users[username] = {'products': {}}
+            json.dump(cart_users, f)
 
 
 if __name__ == "__main__":
@@ -147,3 +168,4 @@ if __name__ == "__main__":
     ]
 
     print(filtering_category(DATABASE, 'Фрукты', 'price_after', True) == test)  # True
+
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse, HttpResponseNotFound
@@ -5,6 +6,8 @@ from .models import DATABASE
 from logic.services import filtering_category
 from logic.services import view_in_cart, add_to_cart, remove_from_cart
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def products_view(request):
@@ -71,9 +74,11 @@ def shop_view(request):
                                'category': category_key})
 
 
+@login_required(login_url='login:login_view')
 def cart_view(request):
     if request.method == "GET":
-        data = view_in_cart()
+        current_user = get_user(request).username
+        data = view_in_cart(request)[current_user]
         if request.GET.get('format') == 'JSON':
             return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
                                                          'indent': 4})
@@ -91,9 +96,10 @@ def cart_view(request):
         return render(request, "store/cart.html", context={"products": products})
 
 
+@login_required(login_url='login:login_view')
 def cart_add_view(request, id_product):
     if request.method == "GET":
-        result = add_to_cart(id_product)
+        result = add_to_cart(request, id_product)
         if result:
             return JsonResponse({"answer": "Продукт успешно добавлен в корзину"},
                                 json_dumps_params={'ensure_ascii': False})
@@ -105,7 +111,7 @@ def cart_add_view(request, id_product):
 
 def cart_del_view(request, id_product):
     if request.method == "GET":
-        result = remove_from_cart(id_product)
+        result = remove_from_cart(request, id_product)
         if result:
             return JsonResponse({"answer": "Продукт успешно удалён из корзины"},
                                 json_dumps_params={'ensure_ascii': False})
@@ -113,3 +119,74 @@ def cart_del_view(request, id_product):
         return JsonResponse({"answer": "Неудачное удаление из корзины"},
                             status=404,
                             json_dumps_params={'ensure_ascii': False})
+
+
+def coupon_check_view(request, name_coupon):
+    # DATA_COUPON - база данных купонов: ключ - код купона (name_coupon); значение - словарь со значением скидки в процентах и
+    # значением действителен ли купон или нет
+    DATA_COUPON = {
+        "coupon": {
+            "value": 10,
+            "is_valid": True},
+        "coupon_old": {
+            "value": 20,
+            "is_valid": False},
+    }
+    if request.method == "GET":
+        # TODO Проверьте, что купон есть в DATA_COUPON, если он есть, то верните JsonResponse в котором по ключу "discount"
+        # получают значение скидки в процентах, а по ключу "is_valid" понимают действителен ли купон или нет (True, False)
+        if name_coupon in DATA_COUPON:
+            coupon = DATA_COUPON[name_coupon]
+            return JsonResponse({
+                'is_valid': coupon['is_valid'],
+                'discount': coupon['value']
+            })
+        # TODO Если купона нет в базе, то верните HttpResponseNotFound("Неверный купон")
+        return HttpResponseNotFound('Неверный купон')
+
+
+def delivery_estimate_view(request):
+    # База данных по стоимости доставки. Ключ - Страна; Значение словарь с городами и ценами; Значение с ключом fix_price
+    # применяется если нет города в данной стране
+    DATA_PRICE = {
+        "Россия": {
+            "Москва": {"price": 80},
+            "Санкт-Петербург": {"price": 80},
+            "fix_price": 100,
+        },
+    }
+    if request.method == "GET":
+        data = request.GET
+        country = data.get('country')
+        city = data.get('city')
+        # TODO Реализуйте логику расчёта стоимости доставки, которая выполняет следующее:
+        # Если в базе DATA_PRICE есть и страна (country) и существует город(city), то вернуть JsonResponse со словарём, {"price": значение стоимости доставки}
+        if country in DATA_PRICE:
+            if city in DATA_PRICE[country]:
+                return JsonResponse({'price': DATA_PRICE[country][city]['price']})
+        # Если в базе DATA_PRICE есть страна, но нет города, то вернуть JsonResponse со словарём, {"price": значение фиксированной стоимости доставки}
+            else:
+                return JsonResponse({'price': DATA_PRICE[country]['fix_price']})
+        # Если нет страны, то вернуть HttpResponseNotFound("Неверные данные")
+        else:
+            return HttpResponseNotFound('Неверные данные')
+
+
+@login_required(login_url='login:login_view')
+def cart_buy_now_view(request, id_product):
+    if request.method == "GET":
+        result = add_to_cart(request, id_product)
+        if result:
+            return redirect("store:cart_view")
+
+        return HttpResponseNotFound("Неудачное добавление в корзину")
+
+
+def cart_remove_view(request, id_product):
+    if request.method == "GET":
+        result = remove_from_cart(request, id_product)  # TODO Вызвать функцию удаления из корзины
+        if result:
+            return redirect("store:cart_view")  # TODO Вернуть перенаправление на корзину
+
+        return HttpResponseNotFound("Неудачное удаление из корзины")
+
